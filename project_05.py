f7aaeab643086a89df4bfa70eb9aa40c37e54af6
seltamouni <https://github.com/seltamouni>
2022-06-27 16:46:17 +0100

    project 05

---
+++ project_05.py
@@ -0,0 +1,9 @@
+def divTwoNumbers(number1,number2):
+    for i in range(1,101):
+        if number1%i ==0 and number2%i ==0:
+            print(i)
+
+
+div1 = int(input("enter the first number :"))
+div2 = int(input("enter the second number :"))
+divTwoNumbers(div1,div2)
